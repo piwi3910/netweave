684029d35dfac49b0aff10d43ad0e7ef05299a90
Pascal Watteel <pascal@watteel.com>
2026-04-21 13:40:36 +0400

    [Fix] gofmt: reorder imports in openapi_route_alignment_test

--- internal/server/openapi_route_alignment_test.go
+++ internal/server/openapi_route_alignment_test.go
@@ -6,8 +6,8 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/gin-gonic/gin"
 	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 
